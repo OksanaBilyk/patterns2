47c76974b86219a07c2a9007fb826cf0d26b4e46
Oksana Bilyk <Oksana.Bilyk@NearShorePartner.ch>
2020-12-09 16:10:56 +0200

    Adapter

---
+++ src/net/basepatterns/structural/adapter/DatabaseRunner.java
@@ -0,0 +1,13 @@
+package net.basepatterns.structural.adapter;
+
+public class DatabaseRunner {
+
+	public static void main(String[] args) {
+		Database database = new AdapterJavaToDatabase();
+
+		database.insert();
+		database.update();
+		database.select();
+		database.remove();
+	}
+}
